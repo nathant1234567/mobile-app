bf52c3485265e21c2e925c48f1ab03878b7b253d
NathanT <nathanthompson256@outlook.com>
2025-06-28 12:35:26 +0100

    final change before i created v3 project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import React from 'react'
 
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Highlights from "./components/Highlights.jsx";
 
 const App = () => {
 
   return (
-      <h1 className="text-3xl font-bold underline">
-          Hello world!
-      </h1>
+      <main>
+          <Navbar/>
+          <Hero/>
+          <Highlights/>
+      </main>
+
+
   )
 }
 
